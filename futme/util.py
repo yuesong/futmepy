02a679667bc4152c470e01594ac3c1bfc63c4312
yuesong <yuesong.wang@oracle.com>
2019-03-01 21:59:25 -0500

    add util.sms()

--- futme/util.py
+++ futme/util.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from twilio.rest import Client
+
+from . import datafile
+
+twilio = datafile.load_json('credentials.json')['twilio']
+client = Client(twilio['account_sid'], twilio['auth_token'])
+
 def sorter(item, descending=True):
     val = item['rating'] * 1e15 + item['discardValue'] * 1e12 + item['assetId'] * 1e6 + item['resourceId']
     if descending:
@@ -8,3 +15,6 @@ def sorter(item, descending=True):
 
 def sorter_key(descending=True):
     return lambda x: sorter(x)
+
+def sms(msg):
+    client.messages.create(body=msg, from_=twilio['from'], to=twilio['to'])
